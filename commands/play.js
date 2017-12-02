39fec292a5822dbf9ddefe1876fdcc03cbf32834
Initiols <ouinitiols@gmail.com>
2017-12-02 10:55:47 +0100

    Changes on play command

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,7 @@
 const ytdl = require('ytdl-core');
 
 exports.run = (client, message, args) => {
-  if (!message.member.voiceChannel && client.voiceConnections.size === 0) {
+  if (!message.member.voiceChannel) {
     message.reply('Il faut que vous soyez dans un channel vocal pour exécuter cette commande');
     return;
   } else if (args.length <= 0 || !args[0].startsWith('https://www.youtube.com/watch?v=')) {
@@ -9,27 +9,30 @@ exports.run = (client, message, args) => {
     return;
   }
 
-  const calledVoiceChannel = message.member.voiceChannel
-    || client.voiceConnections.first().channel;
-  const streamOptions = { seek: 0, volume: 0.1 }; // il faut que ces paramètres soient générals
+  const calledVoiceChannel = message.member.voiceChannel;
+  const streamOptions = { seek: 0, volume: 0.1 };
+  // ↑ il faut que ces paramètres soient générals & modifiables
+
   let timesConnected = 0;
 
-  if (timesConnected === 0) { // CREER LES EVENTS EN DEHORS DE TOUSSA
+  if (timesConnected === 0) { // CREER LES EVENTS EN DEHORS DE TOUSSA ???
     calledVoiceChannel.join()
       .then((connection) => {
         let voiceConnection;
-        client.music.set(voiceConnection, connection);
         let queue;
+        client.music.set(voiceConnection, connection);
         client.music.set(queue, []);
         timesConnected += 1;
+
         const stream = ytdl(args[0], { filter: 'audioonly' });
-        const dispatcher = client.music.get(voiceConnection).playStream(stream, streamOptions);
+        const dispatcher = client.music.get(voiceConnection).playStream(stream, streamOptions); // .playStream() not a function
 
         dispatcher.on('end', () => {
           calledVoiceChannel.leave();
         });
         dispatcher.on('error', (e) => {
-          console.log(e); message.channel.send('Une erreur s\'est produite.');
+          console.log(e);
+          message.channel.send('Une erreur s\'est produite.');
         });
       });
   }
@@ -38,7 +41,7 @@ exports.run = (client, message, args) => {
 exports.help = {
   name: 'play',
   aliases: ['p', 'music'],
-  description: 'Joue la musique dans un channel vocal à partir d\'un lien YouTube. Vous devez être dans un channel vocal lorsque vous exécutez cette commande !',
+  description: 'Joue la musique dans un channel vocal à partir d\'un lien YouTube.',
   usage: '/play <Lien YouTube>',
   active: true,
 };
