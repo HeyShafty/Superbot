db28c3a9a8fcf2de3f3d62de81f5183f854d778c
Initiols <ouinitiols@gmail.com>
2017-12-03 19:40:09 +0100

    Deactivated queue command

--- commands/queue.js
+++ commands/queue.js
@@ -1,5 +1,7 @@
 exports.run = (client, message, args) => {
-  message.channel.send(`= Liste des musiques à suivre =\n\n[Faites la commande /play pour en ajouter une]\n\n${client.music.queue.map(c => `${c}`).join('\n')}`, { code: 'asciidoc' });
+  // if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Add some songs to the queue first with ${tokens.prefix}add`);
+  message.channel.send(`= Liste des musiques à suivre =\n\n[Faites la commande /play pour en ajouter une]\n\n${client.music.queue
+    .forEach((c, i) => `${i + 1}. ${c} - Ajoutée par: ${client.music.queue.requester}`).join('\n')}`, { code: 'asciidoc' });
 };
 
 exports.help = {
@@ -7,5 +9,5 @@ exports.help = {
   aliases: ['q', 'list'],
   description: 'Affiche la liste des musiques à venir',
   usage: '/queue',
-  active: true,
+  active: false,
 };
