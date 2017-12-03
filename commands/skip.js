ba1bf2a41648cb568feb963ad19063d525706e15
Initiols <ouinitiols@gmail.com>
2017-12-03 23:04:08 +0100

    Added skip.js

---
+++ commands/skip.js
@@ -0,0 +1,11 @@
+exports.run = (client, message, args) => {
+  // le code ici
+};
+
+exports.help = {
+  name: 'skip',
+  aliases: ['s', '>'],
+  description: 'Passe à la prochaine musique de la queue',
+  usage: '/skip',
+  active: false, // NE PAS OUBLIER DE CHANGER LOL
+};
