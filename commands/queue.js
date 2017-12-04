229db98c3c1afac59762170c5dd41371ef0c4546
Initiols <ouinitiols@gmail.com>
2017-12-04 21:36:25 +0100

    Modifications

--- commands/queue.js
+++ commands/queue.js
@@ -1,7 +1,14 @@
 exports.run = (client, message, args) => {
-  // if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Add some songs to the queue first with ${tokens.prefix}add`);
-  message.channel.send(`= Liste des musiques à suivre =\n\n[Faites la commande /play pour en ajouter une]\n\n${client.music.queue
-    .forEach((c, i) => `${i + 1}. ${c} - Ajoutée par: ${client.music.queue.requester}`).join('\n')}`, { code: 'asciidoc' });
+  if (client.music.queue[message.guild.id].songs[0] === undefined) return message.channel.send('Il n\'y a aucune musique dans la queue');
+  message.channel.send(
+    `= Liste des musiques à suivre =\n
+    \n[Liste ${(client.music.queue[message.guild.id].songs.length > 1 ? `des ${client.music.queue[message.guild.id].songs.length} musiques` : 'de la seule musique')} à venir pour le serveur ${message.guild.name}]\n
+    \n${client.music.queue[message.guild.id].songs.forEach((song, i) => `${i + 1}. ${song.title} - Ajoutée par: ${song.requester} [\`${song.url}\`]`).join('\n')}`,
+    {
+      code: 'asciidoc',
+      // split: true, // { char: '```asciidoc' } ${(client.music.queue[message.guild.id].songs.length > 15 ? ' *[Seulement les 15 suivantes sont montrées]*' : '')},
+    },
+  );
 };
 
 exports.help = {
@@ -9,5 +16,5 @@ exports.help = {
   aliases: ['q', 'list'],
   description: 'Affiche la liste des musiques à venir',
   usage: '/queue',
-  active: false,
+  active: true,
 };
