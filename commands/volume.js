bea2df414a0cd7ef788e0dfb3c184bd8daa3b7ec
Initiols <ouinitiols@gmail.com>
2017-12-07 21:33:53 +0100

    Working volume cmd

---
+++ commands/volume.js
@@ -0,0 +1,21 @@
+exports.run = (client, message, args) => {
+  /*
+  .volumeDecibels
+  */
+  if (!message.guild.voiceConnection) {
+    message.channel.send('Le bot n\'est pas connecté');
+  } else if (args.length <= 0) {
+    message.channel.send(`Le volume actuel est de \`${message.guild.voiceConnection.dispatcher.volume}\``);
+  } else {
+    message.guild.voiceConnection.dispatcher.setVolume(args[0])
+    message.channel.send(`Le nouveau volume est de \`${args[0]}\``);
+  }
+};
+
+exports.help = {
+  name: 'volume',
+  aliases: ['v'],
+  description: 'Affiche, change le volume',
+  usage: '/volume [volume]',
+  active: true,
+};
