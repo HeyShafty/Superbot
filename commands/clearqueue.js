2a45cdb4f0b2bd71aefdf3aa1e35ce4ad70aeff1
Initiols <ouinitiols@gmail.com>
2017-12-10 21:02:23 +0100

    Added /clearqueue

---
+++ commands/clearqueue.js
@@ -0,0 +1,13 @@
+exports.run = (client, message, args) => {
+  if (client.music[message.guild.id].songs[0] === undefined) return message.channel.send('Il n\'y a aucune musique dans la queue');
+  client.music[message.guild.id].songs.splice(0, client.music[message.guild.id].songs.length);
+  message.channel.send('Toutes les musiques ont étés enlevées de la queue');
+};
+
+exports.help = {
+  name: 'clearqueue',
+  aliases: ['empty', 'cq', 'cc'],
+  description: 'Vide la queue des musiques',
+  usage: '/clearqueue',
+  active: true,
+};
