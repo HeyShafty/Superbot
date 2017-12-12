8d234ea1e089b481223e388aea77f074bfa4ca38
Initiols <ouinitiols@gmail.com>
2017-12-12 23:09:39 +0100

    Added file

---
+++ commands/playingTime.js
@@ -0,0 +1,19 @@
+exports.run = (client, message, args) => {
+  // le code ici
+};
+
+exports.help = {
+  name: '',
+  aliases: ['', ''],
+  description: '',
+  usage: '',
+  active: false, // !!!
+};
+
+exports.flags = {
+  a: {
+    description: '',
+    usage: '',
+    active: false, // !!!
+  },
+};
